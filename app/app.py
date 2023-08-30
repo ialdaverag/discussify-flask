ec155f4e774ea2ce9326fd3f0555bcea1b7aaa82
ialdaverag <elite-a64@outlook.com>
2023-08-30 17:45:31 -0600

    configuring app

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
 
+from config.config import Config
+
 
 def create_app():
     app = Flask(__name__)
 
+    config_app(app)
+
     return app
+
+
+def config_app(app):
+    app.config.from_object(Config)
