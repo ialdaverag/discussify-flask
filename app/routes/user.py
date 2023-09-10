b546be7fcf34f1fd79cf6b154e0f198118803706
ialdaverag <elite-a64@outlook.com>
2023-09-09 18:43:18 -0600

    implementing the read user bookmarks function

--- app/routes/user.py
+++ app/routes/user.py
@@ -2,8 +2,10 @@ from http import HTTPStatus
 
 from flask import Blueprint
 from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
 
 from models.user import User
+
 from schemas.user import user_schema
 from schemas.user import users_schema
 from schemas.community import communities_schema
@@ -51,3 +53,12 @@ def read_user_posts(username):
         return {'message': 'User not found'}, HTTPStatus.NOT_FOUND
     
     return posts_schema.dump(user.posts)
+
+
+@user_routes.route('/bookmarks', methods=['GET'])
+@jwt_required()
+def read_user_bookmarks():
+    current_user = get_jwt_identity()
+    current_user = User.query.get(current_user)
+
+    return posts_schema.dump(current_user.bookmarks)
