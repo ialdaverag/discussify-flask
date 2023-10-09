747de79803eaff2e91fe699e04e65c38aed6e28b
ialdaverag <elite-a64@outlook.com>
2023-10-09 14:32:13 -0600

    activate the confirm email feature

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -46,15 +46,15 @@ def signup():
     
     user = User(**data)
 
-    #token = generate_verification_token(user.email, salt='activate')
-    #subject = 'Please confirm your registration'
-    #link = url_for('auth_routes.confirm_email', token=token, _external=True)
+    token = generate_verification_token(user.email, salt='activate')
+    subject = 'Please confirm your registration'
+    link = url_for('auth_routes.confirm_email', token=token, _external=True)
 
-    #send_email(
-    #    to=user.email, 
-    #    subject=subject, 
-    #    template=render_template('email/confirmation.html', link=link)
-    #)
+    send_email(
+        to=user.email, 
+        subject=subject, 
+        template=render_template('email/confirmation.html', link=link)
+    )
     
     db.session.add(user)
     db.session.commit()
@@ -83,8 +83,8 @@ def login():
     if not check_password(password, user.password):
         return {'message': 'Incorrect password'}, HTTPStatus.UNAUTHORIZED
     
-    #if user.is_verified is False:
-    #        return {'message': 'The user account is not activated yet'}, HTTPStatus.FORBIDDEN
+    if user.is_verified is False:
+            return {'message': 'The user account is not activated yet'}, HTTPStatus.FORBIDDEN
     
     access_token = create_access_token(identity=user.id)
     
