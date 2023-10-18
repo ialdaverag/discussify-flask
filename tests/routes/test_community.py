00c6be6227d0257c8f8cf76bebf5bd8a522e256c
ialdaverag <ialdaverag@outlook.es>
2023-10-18 16:18:11 -0600

    add the update_community function tests

--- tests/routes/test_community.py
+++ tests/routes/test_community.py
@@ -12,22 +12,18 @@ class CommunityTests(BaseTestCase):
     def setUp(self) -> None:
         super().setUp()
 
-        user = User(
-            username='test', 
-            email='test@example.com', 
-            password=hash_password('TestPassword123')
-        )
-        db.session.add(user)
-        db.session.commit()
+        user = User(username='test', email='test@example.com', password=hash_password('TestPassword123'))
+        community = Community(name='TestCommunity', about='A community created for tests', owner=user)
+        user2 = User(username='test2', email='test2@example.com', password=hash_password('TestPassword132'))
+        community2 = Community(name='TestCommunity2',about='Another community created for tests', owner=user2)
 
-        community = Community(
-            name='TestCommunity',
-            about='A community created for tests'
-        )
-        db.session.add(community)
+        db.session.add_all([user, community, user2, community2])
         db.session.commit()
 
         self.user = user
+        self.community = community
+        self.user2 = user2
+        self.community2 = community2
 
 
 class CreateCommunityTests(CommunityTests):
@@ -37,7 +33,7 @@ class CreateCommunityTests(CommunityTests):
         route = 'community/'
         content_type='application/json'
         json = {
-            'name': 'TestCommunity2',
+            'name': 'NewCommunity',
             'about': 'A new community for tests'
         }
         headers={
@@ -117,3 +113,96 @@ class CreateCommunityTests(CommunityTests):
         )
 
         self.assertEqual(400, response.status_code)
+
+class UpdateCommunityTests(CommunityTests):
+    def test_update_community(self):
+        access_token = login(user=self.user)
+        community = self.community.name
+        
+        route = f'community/{community}'
+        content_type='application/json'
+        json = {
+            'name': 'UpdatedCommunity',
+            'about': 'A updated community for tests'
+        }
+        headers={
+            'Authorization': f'Bearer {access_token}'
+        }
+
+        response = self.client.patch(
+            route, 
+            content_type=content_type, 
+            json=json,
+            headers=headers
+        )
+
+        self.assertEqual(200, response.status_code)
+
+    def test_update_community_incorrect_owner(self):
+        access_token = login(user=self.user2)
+        community = self.community.name
+
+        route = f'community/{community}'
+        content_type='application/json'
+        json = {
+            'name': 'UpdatedCommunity',
+            'about': 'A updated community for tests'
+        }
+        headers={
+            'Authorization': f'Bearer {access_token}'
+        }
+
+        response = self.client.patch(
+            route, 
+            content_type=content_type, 
+            json=json,
+            headers=headers
+        )
+
+        self.assertEqual(403, response.status_code)
+
+    def test_update_community_incorrect_name(self):
+        access_token = login(user=self.user)
+        community = self.community.name
+        
+        route = f'community/{community}'
+        content_type='application/json'
+        json = {
+            'name': 'Updated Community',
+            'about': 'A updated community for tests'
+        }
+        headers={
+            'Authorization': f'Bearer {access_token}'
+        }
+
+        response = self.client.patch(
+            route, 
+            content_type=content_type, 
+            json=json,
+            headers=headers
+        )
+
+        self.assertEqual(400, response.status_code)
+    
+    def test_update_community_name_taken(self):
+        access_token = login(user=self.user2)
+        
+        route = f'community/{self.community2.name}'
+        content_type='application/json'
+        json = {
+            'name': 'TestCommunity',
+            'about': 'A new community for tests'
+        }
+        headers={
+            'Authorization': f'Bearer {access_token}'
+        }
+
+        response = self.client.patch(
+            route, 
+            content_type=content_type, 
+            json=json,
+            headers=headers
+        )
+
+        self.assertEqual(400, response.status_code)
+    
