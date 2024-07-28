6dff6c2c558087f1a0d907176e245132077ca752
Ivan <elite-a64@outlook.com>
2024-07-28 17:45:10 -0600

    read user downvotes posts

---
+++ tests/routes/users/test_read_downvoted_comments.py
@@ -0,0 +1,68 @@
+# tests
+from tests.base.base_test_case import BaseTestCase
+
+# factories
+from tests.factories.user_factory import UserFactory
+from tests.factories.comment_factory import CommentFactory
+
+# utils
+from tests.utils.tokens import get_access_token
+
+
+class TestReadDownvotedComments(BaseTestCase):
+    route = '/user/comments/downvoted'
+
+    def test_read_downvoted_comments(self):
+        # Create a user
+        user = UserFactory()
+
+        # Create some comments
+        comments = CommentFactory.create_batch(5)
+
+        # Make the user subscribe to the comments' communities
+        for comment in comments:
+            user.subscribe_to(comment.post.community)
+
+        # Make the user downvote the comments
+        for comment in comments:
+            user.downvote_comment(comment)
+
+        # Get user access token
+        access_token = get_access_token(user)
+
+        # Get user downvoted comments
+        response = self.client.get(
+            f'/user/comments/downvoted',
+            headers={'Authorization': f'Bearer {access_token}'}
+        )
+
+        # Assert that the response status code is 200
+        self.assertEqual(response.status_code, 200)
+
+        # Get response data
+        data = response.json
+
+        # Assert that the response data is a list
+        self.assertIsInstance(data, list)
+
+    def test_read_downvoted_comments_empty(self):
+        # Create a user
+        user = UserFactory()
+
+        # Get user access token
+        access_token = get_access_token(user)
+
+        # Get the user downvoted comments
+        response = self.client.get(
+            f'/user/comments/downvoted',
+            headers={'Authorization': f'Bearer {access_token}'}
+        )
+
+        # Assert that the response status code is 200
+        self.assertEqual(response.status_code, 200)
+
+        # Get response data
+        data = response.json
+
+        # Assert that the response data is an empty list
+        self.assertEqual(data, [])
