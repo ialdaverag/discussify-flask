1d64e9080e23013953b80039ae08178f7dd104e7
Ivan <elite-a64@outlook.com>
2024-08-20 03:25:29 -0600

    Add User create tests

--- tests/managers/user/test_create_user.py
+++ tests/managers/user/test_create_user.py
@@ -1,1 +1,62 @@
-def 
+# Base
+from tests.base.base_test_case import BaseTestCase
+
+# Factories
+from tests.factories.user_factory import UserFactory
+
+# Managers
+from app.managers.user import UserManager
+
+# Errors
+from app.errors.errors import NameError
+
+
+class TestCreateUser(BaseTestCase):
+    def test_create_user(self):
+        # Define user data
+        data = {
+            'username': 'test_user',
+            'email': 'user@email.com',
+            'password': 'password',
+        }
+
+        # Create the user
+        user = UserManager.create(data)
+
+        # Assert that the user was created
+        self.assertIsNotNone(user)
+
+        # Assert that the user data is correct
+        self.assertEqual(user.username, data['username'])
+        self.assertEqual(user.email, data['email'])
+
+
+    def test_create_user_already_existent_username(self):
+        # Create a user
+        user = UserFactory()
+
+        # Define user data
+        data = {
+            'username': user.username,
+            'email': 'user@email.com',
+            'password': 'password',
+        }
+
+        # Try to create the user
+        with self.assertRaises(NameError):
+            UserManager.create(data)
+
+    def test_create_user_already_existent_email(self):
+        # Create a user
+        user = UserFactory()
+
+        # Define user data
+        data = {
+            'username': 'test_user',
+            'email': user.email,
+            'password': 'password',
+        }
+
+        # Try to create the user
+        with self.assertRaises(NameError):
+            UserManager.create(data)
